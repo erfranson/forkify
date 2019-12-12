bf456e0e01db625eebcd89212cb4084f79ef2c1b
Ethan Franson <ethanfranson2@gmail.com>
2019-12-12 15:00:20 -0700

    setting up webpack-config, webpack-cli, webpack-dev-server, html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// in webpack there are 4 core concepts, the entry point, the output, loaders, and plugins
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'), //this is where we want the our path to look
+        filename: 'js/bundle.js' // this is the file we want it to look in for changes
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
+}
