cf6d829145301a6463d19303663d3ab347daff5e
Ethan Franson <ethanfranson2@gmail.com>
2019-12-18 16:44:41 -0700

    babel install

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-// Global app controller
-import num from './test';
+import string from './models/Search';
 
-console.log(`I imported ${num} from another module! test.js`);
+// import * as searchView from './views/searchView';  import all statement 
+import { add, multiply, ID } from './views/searchView';
+
+console.log(`${string}, This is a function ${add(1,2)}, here is a multiply function ${multiply(ID,4)}, here is an id ${ID}`);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // in webpack there are 4 core concepts, the entry point, the output, loaders, and plugins
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['./src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'), //this is where we want the our path to look
         filename: 'js/bundle.js' // this is the file we want it to look in for changes
@@ -15,5 +15,16 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 }
