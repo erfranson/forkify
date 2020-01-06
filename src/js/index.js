d907b9e849da9fcd122f6fd613603adc06100f7e
Ethan Franson <ethanfranson2@gmail.com>
2020-01-06 12:44:44 -0700

    importing axios, and starting to build the search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-import string from './models/Search';
+import axios from 'axios';
+import Search from './models/Search';
 
-// import * as searchView from './views/searchView';  import all statement 
-import { add, multiply, ID } from './views/searchView';
-
-console.log(`${string}, This is a function ${add(1,2)}, here is a multiply function ${multiply(ID,4)}, here is an id ${ID}`);
+const pizza = new Search('pizza');
+console.log(pizza)
+pizza.getResults();
